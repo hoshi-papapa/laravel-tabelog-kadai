d9c79dba1ecf13f2a3fbc24df7f8ebb9faa82e4d
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-13 21:16:37 +0900

    faviconの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
 
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
