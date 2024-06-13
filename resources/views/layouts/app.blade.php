b377290f355a0d84104755ac2707fa696163c231
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-13 20:56:09 +0900

    laravel/uiを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
+
         {{-- Bootstrap --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         
