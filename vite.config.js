ca63fb86a30b201b18b776991ba772dd9fe047f5
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-16 02:33:55 +0900

    Update Vite config to disable modulepreload

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,13 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: undefined,
-            },
-        },
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]',
+            }
+        }
     },
+    modulePreload: {
+        polyfill: false,
+    }
 });
