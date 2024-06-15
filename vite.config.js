7906429c54d026c7080ed90db5fce09939724e77
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-15 11:44:05 +0900

    ヘッダーがおかしいので、設定ファイルを変更

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: undefined // Disable modulepreload
-            }
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // Disable modulepreload
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         }
+      }
     }
+  }
 });
