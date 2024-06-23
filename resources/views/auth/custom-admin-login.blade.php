fff218daeee0ba51b7b7b0cdb66e9f3c7dd14d77
hoshi-papapa <danchicchichichi@gmail.com>
2024-06-23 22:18:56 +0900

    管理者ログインページ編集

--- resources/views/auth/custom-admin-login.blade.php
+++ resources/views/auth/custom-admin-login.blade.php
@@ -5,7 +5,7 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-4">
+            <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">NAGOYAMESHI管理者ログインページ</div>
                     <div class="card-body">
